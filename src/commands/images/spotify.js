26737d5712e29fdf203c7bceb2fbc2590704e89a
Itz-Daniel <81714952+Itz-Daniel@users.noreply.github.com>
2021-07-09 22:29:05 +0100

    Add files via upload

---
+++ src/commands/images/spotify.js
@@ -0,0 +1,48 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class SpotifyCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'spotify',
+      aliases: ['spy'],
+      usage: 'spotify',
+      description: 'shows info about someone who is listening to spotify',
+      examples: ['spotify @User'],
+      type: client.types.IMAGES
+    });
+  }
+     async run (message, args) {
+      let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+  
+      if (!user.presence.activities.length) {
+        return this.sendErrorMessage(message, 0, "User isnt listening to spotify")
+      }
+       
+user.presence.activities.forEach((activity) => {
+
+if (activity.type === 'LISTENING' && activity.name === 'Spotify' && activity.assets !== null) {
+
+                let trackIMG = `https://i.scdn.co/image/${activity.assets.largeImage.slice(8)}`;
+                let trackURL = `https://open.spotify.com/track/${activity.syncID}`;
+
+                let trackName = activity.details;
+                let trackAuthor = activity.state;
+                let trackAlbum = activity.assets.largeText;
+
+                trackAuthor = trackAuthor.replace(/;/g, ",")
+
+                const embed = new MessageEmbed()
+                    .setAuthor('Spotify Track Info', 'https://cdn.discordapp.com/emojis/408668371039682560.png')
+                    .setColor("GREEN")
+                    .setThumbnail(trackIMG)
+                    .addField('Song Name', trackName, true)
+                    .addField('Album', trackAlbum, true)
+                    .addField('Author', trackAuthor, false)
+                    .addField('Listen to Track', `${trackURL}`, false)
+                    .setFooter(user.displayName, user.user.displayAvatarURL({ dynamic: true }))
+                message.channel.send(embed);
+            }
+        })
+    }
+}
