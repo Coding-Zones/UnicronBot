9fe817e4b2c93cb69aac48ea0c14cc2278c8aec5
Itz-Daniel <81714952+Itz-Daniel@users.noreply.github.com>
2021-07-09 22:41:16 +0100

    Add files via upload

---
+++ src/commands/levels/delrep.js
@@ -0,0 +1,23 @@
+const Command = require('../Command.js');
+const db = require('quick.db');
+
+module.exports = class DelrepCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'delrep',
+      aliases: ['dr'],
+      usage: 'clearvouches @user',
+      description: 'Clear vouches of a User',
+      ownerOnly: true,
+      type: client.types.LEVELS
+    });
+  }
+  async run(message) {
+   let user = message.mentions.users.first()
+   let reps = await db.get(`userthanks_${user.id}`)
+   if(!user) return this.sendErrorMessage(message, 0, "mention a user to delete reputation")
+    
+    db.delete(`userthanks_${user.id}`, reps)
+    return message.react('👌')
+    
+}};
